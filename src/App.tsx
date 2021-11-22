07d9f3a5fd443ff65a3291452bb1d341cffeb665
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-11-22 14:57:26 +0500

    Integrated compilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PlatformX - Online Editor</h1>
+
+      <div className="sec-widget" data-widget="58da4c9cd8e023cfeb6e4abd4edd2c08"></div>
     </div>
   );
 }
