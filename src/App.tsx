9ec185d03235a966c2b848b114d8bdc3de90ed16
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-11-23 16:12:50 +0500

    Integrating compiler :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>PlatformX - Online Editor</h1>
-
-      <div className="sec-widget" data-widget="58da4c9cd8e023cfeb6e4abd4edd2c08"></div>
+      {/* for dark theme  */}
+      {/*  add data-theme="dark" */}
+      <div
+        className="sec-widget"
+        data-widget="58da4c9cd8e023cfeb6e4abd4edd2c08"
+        data-theme="dark"
+        data-id="58da4c9cd8e023cfeb6e4abd4edd2c08"
+        data-custom-data="user_id={32432}"
+      ></div>
+      <div id="result"></div>
     </div>
   );
 }
