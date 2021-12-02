7af3b031e2ef9304b9c5a84e92b76bb7480f6ad1
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-12-03 02:01:33 +0500

    Done with online editor

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,20 +1,54 @@
 import React, { FC, useEffect, useState } from "react";
 import "./App.css";
 import axios from "./Utils/Axios";
+import { useTimer } from "react-timer-hook";
+
+type Props = {
+  expiryTimestamp: any;
+};
+
+const MyTimer: FC<Props> = ({ expiryTimestamp }) => {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({
+    expiryTimestamp,
+    onExpire: () => console.warn("onExpire called"),
+  });
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h5 style={{ color: "white" }}>Timer</h5>
+      <div style={{ color: "white" }}>
+        <span style={{ color: "white" }}>{days}</span>:
+        <span style={{ color: "white" }}>{hours}</span>:
+        <span style={{ color: "white" }}>{minutes}</span>:
+        <span style={{ color: "white" }}>{seconds}</span>
+      </div>
+    </div>
+  );
+};
 
 type props = {
   name: string;
   description: string;
 };
 const Editor: FC<props> = ({ name, description }) => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 1200); // 10 minutes timer
   const handleSubmit = () => {
     // get id from local storage
     const api_submission_id = localStorage.getItem("api_submission_id");
     const submission_id = localStorage.getItem("submission_id");
-    console.log(api_submission_id, submission_id);
 
     axios
-      .post("/api/test/create/", {
+      .post("/api/submission/", {
         fyp_id: "15383966-2043-4899-9a31-bf2e75616316",
         submission_id: api_submission_id,
       })
@@ -24,35 +58,42 @@ const Editor: FC<props> = ({ name, description }) => {
         }
       })
       .catch((error) => {
-        alert(error.response.data.errro);
+        console.log(error);
+        alert(error.response.data.error);
       });
   };
 
   return (
-    <div className="App container-fluid" style={{ marginLeft: 30 }}>
-      <div
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 20,
-          padding: 10,
-        }}
-      >
-        <h1
-          style={{
-            color: "white",
-            textAlign: "center",
-          }}
-        >
-          PlatformX
-        </h1>
+    <div className="App container-fluid">
+      <div style={{ flex: 1, flexDirection: "row" }}>
+        <div className="row">
+          <div className="col-11 d-flex justify-content-center">
+            <div
+              style={{
+                marginTop: 4,
+              }}
+            >
+              <h1
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                }}
+              >
+                PlatformX
+              </h1>
+            </div>
+          </div>
+          <div className="col-1 d-flex justify-content-center align-items-end">
+            <MyTimer expiryTimestamp={time} />
+          </div>
+        </div>
       </div>
-      <div style={{ marginLeft: 40 }}>
-        <h2 style={{ color: "white", fontSize: 20 }}>Problem</h2>
-        <p style={{ color: "white" }}>Tower of Hanoi</p>
+      <div style={{ marginLeft: 0 }}>
+        <h2 style={{ color: "white" }}>Problem</h2>
+        <h4 style={{ color: "#98a1a6", fontSize: 22 }}>Tower of Hanoi</h4>
       </div>
-      <div>
-        <p style={{ color: "white" }}>
+      <div style={{ marginLeft: 20 }}>
+        <p style={{ color: "#9ba5ab" }}>
           There are three towers. The objective of the game is to move all the
           disks over to tower #3, but you can't place a larger disk onto a
           smaller disk. To play the game or learn more about the Tower of Hanoi,
